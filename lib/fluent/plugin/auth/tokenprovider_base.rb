c075d802f44b292dbff14ef02fcbdc0d0bdaa104
Tanmaya Panda <tanmayapanda@microsoft.com>
2025-09-25 17:49:04 +0530

    fixed ruby version error

--- lib/fluent/plugin/auth/tokenprovider_base.rb
+++ lib/fluent/plugin/auth/tokenprovider_base.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'logger'
-require 'fluent/plugin/kusto_constants'
+require_relative '../kusto_constants'
 
 # AbstractTokenProvider defines the interface and shared logic for all token providers.
 # Enhanced with retry logic and better token expiry management to prevent timeout issues.
